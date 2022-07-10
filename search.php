76770bd5921c77fbb5fef2b5b61bba4febe0eaa9
Steven Buxton <breathesteve@gmail.com>
2022-07-10 16:49:52 -0500

    init

---
+++ search.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <title>Search Results</title>
+    <meta charset="utf-8">
+    <meta name = "format-detection" content = "telephone=no" />
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/superfish.css">
+    <link href='//fonts.googleapis.com/css?family=Asap:400,700' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery-migrate-1.1.1.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/script.js"></script>
+    <script src="js/jquery.equalheights.js"></script>
+    <script src="js/superfish.js"></script>
+    <script src="js/jquery.mobilemenu.js"></script>
+    <script src="js/jquery.ui.totop.js"></script>
+    <script>
+        $(window).load( function(){
+            $().UItoTop({ easingType: 'easeOutQuart' });
+        });
+    </script>
+    <script src="search/search.js"></script>
+  <!--[if lt IE 8]>
+      <div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/images/upgrade.jpg"border="0"alt=""/></a></div>  
+  <![endif]-->
+  <!--[if lt IE 9]>
+     
+    <link rel="stylesheet" href="css/ie.css" >
+    <script src="js/html5shiv.js"></script>
+  <![endif]-->
+</head>
+<body>    
+<!--==============================header=================================-->
+<header>
+    <div class="container_12">
+        <div class="row">
+            <div class="grid_12">
+                <h1><a href="index.html">Tranzit</a><span>We are always on the move</span></h1>
+                <nav>
+                    <ul class="sf-menu">
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="index-1.html">About</a>
+                            <ul>
+                                <li><a href="#">Our vision</a></li>
+                                <li><a href="#">News</a>
+                                    <ul>
+                                        <li><a href="#">Latest events</a></li>
+                                        <li><a href="#">Fresh links</a></li>
+                                        <li><a href="#">Forum</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Leadership</a></li>
+                                <li><a href="#">T Projects</a></li>
+                           </ul>
+                        </li>
+                        <li><a href="index-2.html">Services</a></li>
+                        <li><a href="index-3.html">Blog</a></li>
+                        <li><a href="index-4.html">Contacts</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+  <!--==============================content=================================-->
+<div class="content">
+    <div class="container_12">
+        <div class="row">
+            <div class="grid_12">
+                <h2>Search result:</h2>
+                <div id="search-results"></div>
+            </div>
+        </div>      
+    </div>
+</div> 
+<!--=======footer=================================-->
+<footer>
+    <div class="bord-2">
+        <div class="container_12">
+            <div class="row">
+                <div class="fright">
+                    <ul class="list-services clearfix">
+                        <li><a class="list-services-1" href="#"><span class="sp-1"></span><span class="sp-2"></span></a></li>
+                        <li><a class="list-services-2" href="#"><span class="sp-1"></span><span class="sp-2"></span></a></li>
+                        <li><a class="list-services-3" href="#"><span class="sp-1"></span><span class="sp-2"></span></a></li>
+                        <li><a class="list-services-4" href="#"><span class="sp-1"></span><span class="sp-2"></span></a></li>
+                    </ul>
+                </div>
+                <div class="fleft">
+                    <a href="index.html" class="foot-logo">Tranzit</a>
+                    <span class="text-1">&copy; 2013 | <a href="index-5.html" class="h-underline">Privacy Policy</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
